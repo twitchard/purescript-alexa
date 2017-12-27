05bf756e8121dfe6a34d698ef99c3a6906b0cb8d
Richard Marmorstein <richard.marmorstein@livestream.com>
2017-12-27 04:40:35 -0500

    Correct lens

--- src/Lens.purs
+++ src/Lens.purs
@@ -5,10 +5,10 @@ import Data.Lens.Record (prop)
 import Data.Maybe (Maybe)
 import Type.Data.Symbol (SProxy(..))
 
-_response :: ∀ r a. Lens' { response :: Maybe a | r} (Maybe a)
+_response :: ∀ r a. Lens' { response :: a | r} a
 _response = prop (SProxy :: SProxy "response")
 
-_outputSpeech :: ∀ r. Lens' { outputSpeech :: String | r} String
+_outputSpeech :: ∀ r a. Lens' { outputSpeech :: a | r} a
 _outputSpeech = prop (SProxy :: SProxy "outputSpeech")
 
 _text :: ∀ r. Lens' { text :: String | r} String
