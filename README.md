40d0eff5a4cb28f61ca331a680996537f0d7a426
Richard Marmorstein <richard.marmorstein@livestream.com>
2018-02-16 19:00:47 -0500

    Let's get started adding some documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+This library contains incomplete type definitions for version 1.0 of the request and response JSON types of the [Alexa Skills API](https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html).
 
+Notably, it lacks definitions for the AudioPlayer and PlaybackController request types. However, it should be enough to get you started making a basic skill with Alexa.
+
+Contributions are welcome!
--- src/LanguageModel.purs
+++ src/LanguageModel.purs
@@ -1,5 +1,14 @@
 module Amazon.Alexa.Skill.LanguageModel where
 
+{- LanguageModel is the type of the object that you provide to Amazon
+   in order to define the voice interface for your skill.
+
+   If you are using the [ASK CLI](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html) tools, you
+   should serialize an object of type { interactionModel :: { languageModel :: LanguageModel } } to JSON and place it in (for example) /models/en-US.json.
+
+   Your language model can be deployed with the command
+   `ask deploy --target model`
+-}
 type LanguageModel =
   { invocationName :: String
   , intents :: Array
